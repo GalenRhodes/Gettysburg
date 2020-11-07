70f8681fea441f76dcc15a673a54cc2d670f694b
Galen Rhodes <galenrhodes@me.com>
2020-11-07 09:14:10 -0500

    Initial Commit

---
+++ Gettysburg/Gettysburg.h
@@ -0,0 +1,18 @@
+//
+//  Gettysburg.h
+//  Gettysburg
+//
+//  Created by Galen Rhodes on 11/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gettysburg.
+FOUNDATION_EXPORT double GettysburgVersionNumber;
+
+//! Project version string for Gettysburg.
+FOUNDATION_EXPORT const unsigned char GettysburgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gettysburg/PublicHeader.h>
+
+
