7ee2378ea886d616fc2933b0a1cafc624db08d78
Galen Rhodes <galenrhodes@me.com>
2021-01-20 07:51:55 -0500

    Test encoding detection.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,18 @@
 
 import PackageDescription
 
-let package = Package(name: "Gettysburg",
-                      platforms: [ .macOS(.v10_15), .tvOS(.v13), .iOS(.v13), .watchOS(.v6) ],
-                      products: [ .library(name: "Gettysburg", targets: [ "Gettysburg" ]), ],
-                      dependencies: [ .package(name: "Rubicon", url: "https://github.com/GalenRhodes/Rubicon", from: "0.3.9"), ],
-                      targets: [ .target(name: "Gettysburg", dependencies: [ "Rubicon" ]), .testTarget(name: "GettysburgTests", dependencies: [ "Gettysburg" ]), ])
+//@f:0
+let package = Package(
+    name: "Gettysburg",
+    platforms: [ .macOS(.v10_15), .tvOS(.v13), .iOS(.v13), .watchOS(.v6) ],
+    products: [
+        .library(name: "Gettysburg", targets: [ "Gettysburg" ]),
+    ],
+    dependencies: [
+        .package(name: "Rubicon", url: "https://github.com/GalenRhodes/Rubicon", from: "0.3.11"),
+    ],
+    targets: [
+        .target(name: "Gettysburg", dependencies: [ "Rubicon" ]),
+        .testTarget(name: "GettysburgTests", dependencies: [ "Gettysburg" ]),
+    ])
+//@f:1
